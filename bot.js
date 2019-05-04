1e867eaaf02daa61c8e8acbe3796c22ed9cbfccb
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-05-04 18:30:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,33 @@
-var Discord = require('discord.js');
-var fs = require('fs');
-var client = new Discord.Client();
-
+const Discord = require('discord.js');// Script = Mixwell
+const client = new Discord.Client();
 client.on('ready', () => {
-  console.log(`Welcome Bro ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
-var prefix = '^'
 
+
+const developers = ["415142691282616330"]
+const adminprefix = "^";
 client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== "415142691282616330") return;
-
-  
-  if (message.content.startsWith(prefix + 'wat')) {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'sp')) {
+    client.user.setGame(argresult, {type:'PLAYING'});
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+   if (message.content.startsWith(prefix + 'wat')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`Watch Now: **${argresult}`)
-} 
-
- 
-  if (message.content.startsWith(prefix + 'lis')) {
-  client.user.setActivity(argresult, {type: 'LISTENING'})
-     console.log('test' + argresult);
-    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
-} 
-
-
-if (message.content.startsWith(prefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-} 
-
-if (message.content.startsWith(prefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+  } else 
+  if (message.content.startsWith(adminprefix + 'sl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
+      message.channel.send(`**Status You ${argresult} **`)
 }
-
-if (message.content.startsWith(prefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/peery13");
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}`)
-} 
-if (message.content.startsWith(prefix + 'ply')) {
-  client.user.setGame(argresult);
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Playing: **${argresult}`)
-} 
-
-
-
 });
 
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);// By Mixwell
